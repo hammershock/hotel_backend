7aa6827f392ab7983ef97339eab3f9aa02ab48c8
hammershock <hammershock@163.com>
2023-12-05 19:36:36 +0800

    update docstring

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 CORS(app)
 
 # 配置 JWT
-app.config['JWT_SECRET_KEY'] = secret_key = os.urandom(24)
-print("secret_key", secret_key)
+app.config['JWT_SECRET_KEY'] = os.urandom(24)
 
 jwt = JWTManager(app)
 
@@ -39,6 +38,12 @@ room_status = {
 @app.route('/room-status/<int:room_id>', methods=['GET'])
 @jwt_required()
 def get_room_status(room_id):
+    """
+    查询房间状态数据<READ>
+    权限设置：用户只能查询自己对应的房间状态，而管理员可以查询所有的房间状态，不为前台提供这项接口
+    :param room_id: 房间号
+    :return:
+    """
     username = get_jwt_identity()  # 查询来自的帐号名
     return jsonify(room_status)
 
@@ -46,6 +51,14 @@ def get_room_status(room_id):
 @app.route('/update-status/<int:room_id>', methods=['POST'])
 @jwt_required()
 def update_status(room_id):
+    """
+    修改房间状态数据<WRITE>
+    权限设置：用户只能修改自己对应的房间状态，而管理员可以修改所有的房间状态，不为前台提供这项接口
+    前置条件：房间号必须存在，修改状态合法
+    后置条件：房间状态已修改
+    :param room_id: 房间号
+    :return:
+    """
     username = get_jwt_identity()  # 查询来自的帐号名
     data = request.json
     room_status.update(data)
@@ -54,6 +67,11 @@ def update_status(room_id):
 
 @app.route('/login', methods=['POST'])
 def login():
+    """
+    用户、前台、管理员的登录
+    <唯一不需要token的操作接口>
+    :return:
+    """
     username = request.json.get('username', None)
     password = request.json.get('password', None)
     role = request.json.get('role', None)
@@ -70,10 +88,16 @@ def login():
 @app.route('/register', methods=['POST'])
 @jwt_required()
 def register():
+    """
+    注册用户帐号，传入json消息体格式如:
+    {'account': {'username': 210, 'password': '666'},
+    'customer': {'idCard': '124', 'phone': '124', 'roomType': '标准间', 'days': 3},
+    'manager' : {这个不重要...}}
+    权限设置：只有[前台，管理员]能进行此操作, 且前台只能注册客户帐号
+    :return:
+    """
     data = request.json
     username = get_jwt_identity()
-    # {'account': {'username': 210, 'password': '666'},
-    # 'customer': {'idCard': '124', 'phone': '124', 'roomType': '标准间', 'days': 3}}
     return jsonify({"msg": "注册成功"}), 201
 
 
