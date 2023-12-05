04e81151bfae6c6220f51827a77a5ae89c7dfcb9
hammershock <hammershock@163.com>
2023-12-05 19:18:52 +0800

    initial commit

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from flask_cors import CORS
 import datetime
 
+
 app = Flask(__name__)
 CORS(app)
 
@@ -14,12 +15,11 @@ print("secret_key", secret_key)
 
 jwt = JWTManager(app)
 
-# 假设的用户数据
+
 users = {
     "111客户": "111",
     "222前台": "222",
     "333管理员": "333"
-    # ... 其他用户 ...
 }
 
 
@@ -30,40 +30,25 @@ room_status = {
         "temperatureMax": 30,
         "fanSpeed": "medium",
         "mode": "cool",
-        "consumption": 0.2,
+        "consumption": 0.0,
         "roomTemperature": 22,
         "rate": 0.1
     }
 
 
-@app.route('/some-protected-route')
-@jwt_required()
-def protected():
-    current_user = get_jwt_identity()
-    # 你现在知道了请求是由谁发出的
-    return jsonify(logged_in_as=current_user), 200
-
-
 @app.route('/room-status/<int:room_id>', methods=['GET'])
 @jwt_required()
 def get_room_status(room_id):
-    # 这里可以添加逻辑来根据 room_id 获取实际的房间状态
-    # 目前我们只是返回假设的数据
-    global ison
-    current_user = get_jwt_identity()
-    # print(room_id, current_user)
+    username = get_jwt_identity()  # 查询来自的帐号名
     return jsonify(room_status)
 
 
 @app.route('/update-status/<int:room_id>', methods=['POST'])
 @jwt_required()
 def update_status(room_id):
-    current_user = get_jwt_identity()
+    username = get_jwt_identity()  # 查询来自的帐号名
     data = request.json
     room_status.update(data)
-
-    print(data, current_user)
-    # 更新房间状态的逻辑...
     return jsonify({"msg": "状态更新成功"}), 200
 
 
@@ -73,21 +58,24 @@ def login():
     password = request.json.get('password', None)
     role = request.json.get('role', None)
 
-    # print(username, password)
     # 验证用户名和密码
     if username+role not in users or users[username+role] != password:
         return jsonify({"msg": "Bad username or password"}), 401
     # 创建 JWT token
     expires = datetime.timedelta(days=7)
     access_token = create_access_token(identity=username, expires_delta=expires)
-    # print(role, username, password, access_token)
     return jsonify(token=access_token)
+
+
+@app.route('/register', methods=['POST'])
+@jwt_required()
+def register():
+    data = request.json
+    username = get_jwt_identity()
+    # {'account': {'username': 210, 'password': '666'},
+    # 'customer': {'idCard': '124', 'phone': '124', 'roomType': '标准间', 'days': 3}}
+    return jsonify({"msg": "注册成功"}), 201
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
